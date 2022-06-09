16c9b561a79232996f7d1b1939657b707b4cefce
Rajsuryan-Singh <rajsuryan.singh@gmail.com>
2022-06-09 15:11:51 +0200

    sort files by time while getting conv1_flow wts

--- scripts/get_conv1_flow_wts.py
+++ scripts/get_conv1_flow_wts.py
@@ -5,12 +5,13 @@ import numpy as np
 from pathlib import Path
 def main():
     checkpoints = glob.glob("../rcgrad_flow/models/*ckpt")
-    for ckpt in checkpoints:
+    checkpoints.sort(key=os.path.getmtime)
+
+    for i, ckpt in enumerate(checkpoints):
         fname = Path(ckpt).stem
-        print(fname)
         state_dict = torch.load(ckpt, map_location=torch.device('cpu'))["state_dict"]
         conv_flow_wts = state_dict['image_encoder.conv1_flow.weight'].numpy()
-        np.save(f"../rcgrad_flow/flow_wts/{fname}.npy", conv_flow_wts.flatten())
+        np.save(f"../rcgrad_flow/flow_wts/{i}-{fname}.npy", conv_flow_wts.flatten())
 
 if __name__ == "__main__":
     os.makedirs("../rcgrad_flow/flow_wts/", exist_ok=True)
