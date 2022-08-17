cf63ec3f4bbe308058f9c9922460fee5d7357ac2
Rajsuryan-Singh <rajsuryan.singh@gmail.com>
2022-08-18 00:58:27 +0200

    change accelerator to auto

--- scripts/train.py
+++ scripts/train.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
             ),
         ],
         gpus=args["num_gpus"],
-        accelerator="dp",
+        accelerator="auto",
         max_epochs=100,
     )
     train_loader = DataLoader(
